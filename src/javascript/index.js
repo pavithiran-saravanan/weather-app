a4e0416491a8cb0f9921bbe536eaaba1b288d916
pavithiran <pavithransaravanan22@gmail.com>
2023-08-04 11:13:32 +0530

    Create a filtered object from the API response with only the required properties.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,7 +5,7 @@ const content = document.querySelector(".content");
 content.textContent = "Hello World";
 
 const apiKey = key;
-const city = "salem";
+const city = "delhi";
 const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
 async function getWeather(src) {
@@ -17,14 +17,54 @@ async function getWeather(src) {
   throw new Error("Invalid Request");
 }
 
-// sync function calling an async func
+function display(obj) {
+  Object.keys(obj).forEach((key) => {
+    const title = document.createElement("h3");
+    title.textContent = key;
+    const value = document.createElement("p");
+    value.textContent = obj[key];
+    content.append(title, value);
+  });
+}
+
+function filterData(data) {
+  const {
+    temp_c: tempC,
+    temp_f: tempF,
+    is_day: isDay,
+    condition,
+    wind_mph: windMph,
+    wind_kph: windKph,
+    humidity,
+    feelslike_c: feelsLikeC,
+    feelslike_f: feelsLikeF,
+  } = data.current;
+  const { name, region, country, localtime } = data.location;
+  return {
+    tempC,
+    tempF,
+    isDay,
+    condition,
+    windMph,
+    windKph,
+    humidity,
+    feelsLikeC,
+    feelsLikeF,
+    name,
+    region,
+    country,
+    localtime,
+  };
+}
+
 function displayWeather(src) {
   const myWeather = getWeather(src);
   myWeather
     .then((data) => {
-      if (data) console.log(data.current);
+      const filteredData = filterData(data);
+      display(filteredData);
     })
     .catch((err) => console.log(err));
 }
 
-displayWeather(url); 
+displayWeather(url);
