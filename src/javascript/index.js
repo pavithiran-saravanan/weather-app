d58eccacf3eeb06f5237ab5f6b99d2c557f1e953
pavithiran <pavithransaravanan22@gmail.com>
2023-08-04 18:38:49 +0530

    Display hourly data for the next 24 hours.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,7 +3,7 @@ import "../stylesheet/style.css";
 
 const content = document.querySelector(".content");
 
-// Create current container and add to content
+// Create current title and current container and add to content
 const currentContainer = document.createElement("div");
 currentContainer.classList.add("current-container");
 const currentTitle = document.createElement("div");
@@ -11,7 +11,7 @@ currentTitle.classList.add("current-title");
 currentTitle.textContent = "Current Weather";
 content.append(currentTitle, currentContainer);
 
-// Create current title and add to current container
+// Create hourly container and hourly title and add to content
 const hourlyContainer = document.createElement("div");
 hourlyContainer.classList.add("hourly-container");
 const hourlyTitle = document.createElement("div");
@@ -20,9 +20,8 @@ hourlyTitle.textContent = "Hourly Weather";
 content.append(hourlyTitle, hourlyContainer);
 
 const apiKey = key;
-const city = "salem";
+const city = "Mawsynram";
 const urlCurrent = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=2`;
-// const urlForecast = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}`;
 
 async function getCurrentWeather(src) {
   const response = await fetch(src, { mode: "cors" });
@@ -33,20 +32,22 @@ async function getCurrentWeather(src) {
   throw new Error("Invalid Request");
 }
 
+// Returns an array of elements which represent current weather and location info
 function getWeatherItems(obj) {
   const weatherItems = [];
-  Object.keys(obj).forEach((key) => {
+  Object.keys(obj).forEach((objKey) => {
     const div = document.createElement("div");
     const title = document.createElement("h4");
-    title.textContent = key;
+    title.textContent = objKey;
     const value = document.createElement("p");
-    value.textContent = obj[key];
+    value.textContent = obj[objKey];
     div.append(title, value);
     weatherItems.push(div);
   });
   return weatherItems;
 }
 
+// Returns an obj containing current weather info filtered from the api response
 function getFilteredData(data) {
   const {
     cloud,
@@ -88,6 +89,7 @@ function getFilteredData(data) {
   };
 }
 
+// Return an array of dom elements where each element contains an hour's weather info
 function getHourlyItems(data) {
   const hourlyItems = [];
   data.forEach((hourData) => {
@@ -110,17 +112,44 @@ function getHourlyItems(data) {
   return hourlyItems;
 }
 
-function getHourlyData(data, day = 0) {
-  const hourlyData = data.forecast.forecastday[day].hour;
-  const filteredData = hourlyData.map((hourData) => {
-    const { temp_c: tempC, temp_f: tempF, condition, time } = hourData;
-    return {
-      tempC,
-      tempF,
-      condition: condition.text,
-      time: time.split(" ")[1],
-    };
-  });
+// Format 24 hour time into 12 hours
+function formatHours(time) {
+  let res = "";
+  if (time === "12") res = `12 PM`;
+  else if (time === "00") res = `12 AM`;
+  else if (time > 12) res = `${time % 12} PM`;
+  else res = `${time} AM`;
+  return res;
+}
+
+// Returns hourly data for the next 24 hours
+function getHourlyData(data) {
+  const localTime = data.location.localtime;
+  const currentHour = localTime.split(" ")[1].split(":")[0];
+  const date = localTime.split(" ")[0];
+
+  const hourlyData = [
+    ...data.forecast.forecastday[0].hour,
+    ...data.forecast.forecastday[1].hour,
+  ];
+  const filteredData = hourlyData
+    .filter((hourData) => {
+      const currDate = hourData.time.split(" ")[0];
+      const currTime = hourData.time.split(" ")[1].split(":")[0];
+      if (date === currDate && currTime > currentHour) return true;
+      if (date !== currDate && currTime <= currentHour) return true;
+      return false;
+    })
+    .map((hourData) => {
+      const { temp_c: tempC, temp_f: tempF, condition, time } = hourData;
+      const hours = time.split(" ")[1].split(":")[0];
+      return {
+        tempC,
+        tempF,
+        condition: condition.text,
+        time: formatHours(hours),
+      };
+    });
   return filteredData;
 }
 
@@ -129,6 +158,7 @@ function displayCurrentWeather(src) {
 
   myWeather
     .then((data) => {
+      console.log(data);
       const filteredData = getFilteredData(data);
       getWeatherItems(filteredData).forEach((item) =>
         currentContainer.append(item)
@@ -139,11 +169,6 @@ function displayCurrentWeather(src) {
   myWeather.then((data) => {
     const todaysData = getHourlyData(data);
     getHourlyItems(todaysData).forEach((item) => {
-      hourlyContainer.append(item);
-    });
-
-    const tomorrowData = getHourlyData(data, 1);
-    getHourlyItems(tomorrowData).forEach((item) => {
       hourlyContainer.append(item);
     });
   });
